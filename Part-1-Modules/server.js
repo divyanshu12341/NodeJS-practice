8ceb7af65fb8d3d40c27e9612bdea5752e0f1de8
Divyanshu Jain <divyanshujain111@gmail.com>
2023-09-09 07:37:22 +0530

    Basics of OS Module

--- Part-1-Modules/server.js
+++ Part-1-Modules/server.js
@@ -1,8 +1,10 @@
-//importing path module
-const path = require("path");
-const directoryName = path.dirname(__dirname);
-console.log(directoryName);
-const extensionName = path.extname(path.join(directoryName,'server.js'));
-console.log(extensionName);
-const absoluteDirectoryName = path.basename(__dirname);
-console.log(absoluteDirectoryName);
+//os module
+const os = require("os");
+console.log("CPU ",os.cpus());
+console.log("Arch ",os.arch());
+console.log("Free memory in OS ",os.freemem());
+console.log("Platform ",os.platform());
+console.log("Temporary directory of OS ",os.tmpdir());
+console.log("Total memory of system is ",os.totalmem());
+console.log("Release of OS is ",os.release());
+console.log("Current user is ",os.userInfo());
