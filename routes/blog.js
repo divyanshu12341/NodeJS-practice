8928184a8f32960c23b1afe949fc136ce9ac6c79
Divyanshu Jain <divyanshujain111@gmail.com>
2023-09-10 12:48:55 +0530

    Created blog api using express-handlebars

--- routes/blog.js
+++ routes/blog.js
@@ -3,8 +3,14 @@ const path = require("path");
 const blogs = require('../data/blogs');
 const router = express.Router();
 router.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,'../templates/index.html'));
+    // res.sendFile(path.join(__dirname,'../templates/index.html'));
+    res.render('home');
 });
+router.get("/blog",(req,res)=>{
+    res.render('blogHome',{
+        blogs:blogs,
+    });
+})
 router.get("/blogs",(req,res)=>{
     blogs.forEach(e =>{
         console.log(e.title);
@@ -16,6 +22,10 @@ router.get("/blogpost/:slug",(req,res)=>{
         return e.slug == req.params.slug;
     });
     console.log(myBlog);
-    res.sendFile(path.join(__dirname,'../templates/blogpage.html'))
+    // res.sendFile(path.join(__dirname,'../templates/blogpage.html'))
+    res.render('blogPage',{
+        title:myBlog[0].title,
+        content:myBlog[0].content
+    })
 })
 module.exports = router;
--- server.js
+++ server.js
@@ -2,8 +2,15 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const PORT = 3000;
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs.engine());
+app.set('view engine', 'handlebars');
 app.use(express.static(path.join(__dirname,'public')));
 app.use('/',require(path.join(__dirname,'routes/blog.js')));
 app.listen(PORT,()=>{
     console.log("APP is listening at port",3000);
 })
+
+
+
+
